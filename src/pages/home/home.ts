6cd830c5f34a3d196403d777ee01c4016e9f4095
Waramun <booms_2p@hotmail.com>
2017-09-21 16:13:46 +0700

    Initail app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
+import { Http } from '@angular/http';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.http.get('https://itunes.apple.com/search?term=eminem')
+    .map(res => res.json())
+    .subscribe(data => {
+      // console.log(data);
+      this.albumLists = data.results;
+      console.log(this.albumLists);
+    });
   }
+
+  albumLists;
 
 }
